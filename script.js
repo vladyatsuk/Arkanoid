c2625c0186c80023d0d8f3aa4141fb31b14ff7d3
vladyatsuk <yatsuk.vladislav@gmail.com>
2022-06-15 16:42:38 +0300

    changed some parameters

--- script.js
+++ script.js
@@ -2,30 +2,23 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-canvas.width = 500;
-canvas.height = 500;
-
-let drawRectangle = function (obj) {
-  context.beginPath();
-  context.rect(obj.x, obj.y, obj.width, obj.height);
-  context.strokeStyle = 'green';
-  context.stroke();
-};
+canvas.width = 800;
+canvas.height = 800;
 
 const player = {
   x: canvas.width / 2,
   y: canvas.height - 15,
   width: 150,
   height: 8,
-  speed: 300,
+  speed: 15,
 };
 const ball = {
   x: canvas.width / 2,
-  y: canvas.height / 2,
+  y: canvas.width / 2,
   width: 10,
   height: 10,
-  speed: 100,
-  angle: Math.Pi / 2,
+  speed: 50,
+  angle: Math.Pi / 4 + (Math.random() * Math.Pi) / 2,
 };
 
 const blocks = [
@@ -35,8 +28,9 @@ const blocks = [
   { x: 200, y: 50, width: 50, height: 10 },
 ];
 
-drawRectangle(player);
-drawRectangle(ball);
-for (const block of blocks) {
-  drawRectangle(block);
-}
+let drawRectangle = function (obj) {
+  context.beginPath();
+  context.rect(obj.x, obj.y, obj.width, obj.height);
+  context.strokeStyle = 'green';
+  context.stroke();
+};
