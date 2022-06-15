29cae594f461e4270694afeb49c536c7abf1a9f7
vladyatsuk <yatsuk.vladislav@gmail.com>
2022-06-15 20:07:55 +0300

    added "isIntersection" function to track ball hits

--- script.js
+++ script.js
@@ -49,4 +49,41 @@ const moveBall = function (currTime = 0) {
   ball.x += deltaInSeconds * ball.speed;
   ball.y -= deltaInSeconds * ball.speed;
 };
+
+function isIntersection(blockA, blockB) {
+  const pointsA = [
+    { x: blockA.x, y: blockA.y },
+    { x: blockA.x + blockA.width, y: blockA.y },
+    { x: blockA.x, y: blockA.y + blockA.height },
+    { x: blockA.x + blockA.width, y: blockA.y + blockA.height },
+  ];
+
+  const pointsB = [
+    { x: blockB.x, y: blockB.y },
+    { x: blockB.x + blockB.width, y: blockB.y },
+    { x: blockB.x, y: blockB.y + blockB.height },
+    { x: blockB.x + blockB.width, y: blockB.y + blockB.height },
+  ];
+
+  for (const pointA of pointsA) {
+    if (
+      blockB.x <= pointA.x &&
+      pointA.x <= blockB.x + blockB.width &&
+      blockB.y <= pointA.y &&
+      pointA.y <= blockB.y + blockB.height
+    )
+      return true;
+  }
+
+  for (const pointB of pointsB) {
+    if (
+      blockA.x <= pointB.x &&
+      pointB.x <= blockA.x + blockA.width &&
+      blockA.y <= pointB.y &&
+      pointB.y <= blockA.y + blockA.height
+    )
+      return true;
+  }
+}
+
 moveBall(0);
