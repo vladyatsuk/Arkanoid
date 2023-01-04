1da3802e9eea3edd4aecf6219fc121f69fd37432
vladyatsuk <yatsuk.vladislav@gmail.com>
2023-01-04 18:02:35 +0200

    returned constants to the beginning of the file

--- script.js
+++ script.js
@@ -12,7 +12,45 @@ const speed = 1.5;
 const playerStartPosX = canvas.width / 2 - 50;
 const playerPosY = canvas.height - 75;
 const ballStartPosY = playerPosY - 15;
+const boost = 5;
+const indent = 50;
 let bricks = [];
+let score = 0;
+let bestScore = 0;
+let levelIndex = 0;
+
+const LEVELS = [
+  [
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ],
+  [
+    [0, 1, 1, 0, 0, 1, 1, 0],
+    [0, 1, 1, 0, 0, 1, 1, 0],
+    [0, 1, 1, 0, 0, 1, 1, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ],
+  [
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 1, 0, 1, 0],
+    [1, 0, 1, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 1, 0, 1],
+    [0, 1, 0, 1, 1, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+  ],
+];
 
 const colors = [
   'silver',
@@ -122,52 +160,15 @@ const brick = {
 };
 
 class Game {
-  score = 0;
-  bestScore = 0;
-  levelIndex = 0;
-  boost = 5;
-  indent = 50;
-  LEVELS = [
-    [
-      [1, 1, 1, 1, 1, 1, 1, 1],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [1, 1, 1, 1, 1, 1, 1, 1],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [1, 1, 1, 1, 1, 1, 1, 1],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-      [1, 1, 1, 1, 1, 1, 1, 1],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-    ],
-    [
-      [0, 1, 1, 0, 0, 1, 1, 0],
-      [0, 1, 1, 0, 0, 1, 1, 0],
-      [0, 1, 1, 0, 0, 1, 1, 0],
-      [0, 0, 0, 1, 1, 0, 0, 0],
-      [0, 1, 0, 0, 0, 0, 1, 0],
-      [0, 0, 1, 0, 0, 1, 0, 0],
-      [0, 0, 0, 1, 1, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0],
-    ],
-    [
-      [0, 0, 0, 1, 1, 0, 0, 0],
-      [0, 0, 1, 0, 0, 1, 0, 0],
-      [0, 1, 0, 1, 1, 0, 1, 0],
-      [1, 0, 1, 0, 0, 1, 0, 1],
-      [1, 0, 1, 0, 0, 1, 0, 1],
-      [0, 1, 0, 1, 1, 0, 1, 0],
-      [0, 0, 1, 0, 0, 1, 0, 0],
-      [0, 0, 0, 1, 1, 0, 0, 0],
-    ],
-  ];
   constructor(ball, player, brick) {
     this.ball = ball;
     this.player = player;
     this.brick = brick;
   }
   drawScore() {
-    if (this.score > this.bestScore) this.bestScore = this.score;
-    scoreLabel.innerHTML = `Score: ${this.score}`;
-    bestScoreLabel.innerHTML = `Best score: ${this.bestScore}`;
+    if (score > bestScore) bestScore = score;
+    scoreLabel.innerHTML = `Score: ${score}`;
+    bestScoreLabel.innerHTML = `Best score: ${bestScore}`;
   }
   draw() {
     this.player.draw('white', 0, 0, canvas.width, canvas.height);
@@ -198,18 +199,18 @@ class Game {
     if (hitFloor) return true;
   }
   showGameStatus() {
-    if (this.score === 100 * (this.levelIndex + 1) * bricks.length) {
-      this.score = 0;
-      this.levelIndex += 1;
-      header.innerHTML = `You won level ${this.levelIndex} :)`;
-      if (this.levelIndex === 3) {
+    if (score === 100 * (levelIndex + 1) * bricks.length) {
+      score = 0;
+      levelIndex += 1;
+      header.innerHTML = `You won level ${levelIndex} :)`;
+      if (levelIndex === 3) {
         header.innerHTML = 'You won the last level :)';
-        this.levelIndex = 0;
+        levelIndex = 0;
       }
       this.reset();
     }
     if (this.isLoss()) {
-      this.levelIndex = 0;
+      levelIndex = 0;
       header.innerHTML = 'You lost :(';
       this.reset();
     }
@@ -219,7 +220,7 @@ class Game {
     bricks = [];
     this.player.x = playerStartPosX;
     this.ball.x =
-      Math.random() * (canvas.width - 2 * this.indent) + this.indent;
+      Math.random() * (canvas.width - 2 * indent) + indent;
     this.ball.y = ballStartPosY;
     for (let y = 0; y < level.length; y++) {
       for (let x = 0; x < level.length; x++) {
@@ -236,20 +237,20 @@ class Game {
     this.player.setControls();
   }
   reset() {
-    this.score = 0;
+    score = 0;
     this.ball.speedX = 0;
     this.ball.speedY = 0;
     this.player.x = playerStartPosX;
     this.player.canLaunchBall = true;
     this.ball.x =
-      Math.random() * (canvas.width - 2 * this.indent) + this.indent;
-    this.init(this.LEVELS[this.levelIndex]);
+      Math.random() * (canvas.width - 2 * indent) + indent;
+    this.init(LEVELS[levelIndex]);
   }
   BounceOffCeiling() {
     const hitCeiling = this.ball.y - this.ball.r + this.ball.speedY <= 0;
     if (hitCeiling) {
       this.ball.speedY *= -1;
-      this.ball.y += this.boost;
+      this.ball.y += boost;
     }
   }
   BounceOffWalls() {
@@ -258,8 +259,8 @@ class Game {
       this.ball.x + this.ball.r + this.ball.speedX >= canvas.width;
     if (hitWalls) {
       if (this.ball.x < canvas.width / 2) {
-        this.ball.x += this.boost;
-      } else ball.x += -this.boost;
+        this.ball.x += boost;
+      } else ball.x += -boost;
       this.ball.speedX *= -1;
     }
   }
@@ -270,7 +271,7 @@ class Game {
       this.ball.x <= this.player.x + this.player.width;
     if (hitPlayer) {
       this.ball.speedY *= -1;
-      this.ball.y -= this.boost;
+      this.ball.y -= boost;
       if (player.moveLeft()) this.ball.speedX = -speed;
       if (player.moveRight()) this.ball.speedX = speed;
     }
@@ -283,7 +284,7 @@ class Game {
         bricks[i].y < this.ball.y + this.ball.r &&
         this.ball.y - this.ball.r < bricks[i].y + bricks[i].height;
       if (bricks[i].active && isHitBrick) {
-        this.score += 100 * (this.levelIndex + 1);
+        score += 100 * (levelIndex + 1);
         bricks[i].active = false;
         this.ball.speedY *= -1;
         break;
@@ -307,5 +308,5 @@ class Game {
 const ball = new Ball(10);
 const player = new Player(100, 20, speed, ball);
 const game = new Game(ball, player, brick);
-game.init(game.LEVELS[game.levelIndex]);
+game.init(LEVELS[levelIndex]);
 setInterval(game.playgame, 1);
